bd828833ac6a909dcbd5f922a4ce4de68fe75b1f
LEMARK-J-MAC <lemark-j-mac@lemark-j-mac.lan>
2019-08-18 00:29:17 -0600

    Updates

---
+++ precache-manifest.59cdb5b5744ca65ee479e5e28efb344f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bad560f70d65077954bb658b9d75cf6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "57adf6a86e645cbaf1b5",
+    "url": "/monsters-rolodex/static/css/main.99bea156.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "57adf6a86e645cbaf1b5",
+    "url": "/monsters-rolodex/static/js/main.7f6f20b3.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
